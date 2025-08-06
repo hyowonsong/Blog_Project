1bcc8945cbbceba57aba28c0eeaf08525c5a4558
hyowonsong <woniwoni30@gmail.com>
2025-08-06 16:16:47 +0900

    wonylog commit first

---
+++ src/main/java/com/wonylog/WonylogApplication.java
@@ -0,0 +1,13 @@
+package com.wonylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonylogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WonylogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wonylog/WonylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonylogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
